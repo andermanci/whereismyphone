3371319ae81c51f8c89f858bf8b6b1f03941d036
andermanci <andermanci6@gmail.com>
2017-04-19 12:04:16 +0200

    Hirugarren igoera, device eta user arteko lotura egina user baten device-ak erakutsi

---
+++ app/models/user.rb
@@ -0,0 +1,52 @@
+
+class User < ApplicationRecord
+  validates :username,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  attr_accessor :password
+  before_save :encrypt_password
+
+
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email, :on => :create
+  validates_presence_of :username, :on => :create
+  validates_uniqueness_of :email
+  validates_uniqueness_of :username
+
+  has_many :devices, dependent: :destroy
+
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def self.authenticate_by_email(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+
+
+
+end
