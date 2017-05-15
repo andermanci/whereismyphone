69b1568d43d098a23e9ff0239d5b12ab02026b93
andermanci <andermanci6@gmail.com>
2017-05-15 12:55:46 +0200

    6igoera change password

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bcrypt-ruby', :require=>'bcrypt'
 # gem 'capistrano-rails', group: :development
 gem 'autoprefixer-rails'
 gem 'fcm'
+gem 'firebase'
 gem 'bootstrap-sass', '3.3.6'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -2,6 +2,7 @@
 class DeviceController < ApplicationController
 
   require 'fcm'
+  require 'firebase'
 
   def selected_device
 
@@ -30,9 +31,14 @@ class DeviceController < ApplicationController
   def activate_GPS
     if current_device
 
-      fcm = FCM.new("AIzaSyAf8cH_rC19EbpWyiFRKezYcMAUG3O4gyA ")
-      options = { collapse_key: "activate_GPS"}
+      fcm = FCM.new('AIzaSyAf8cH_rC19EbpWyiFRKezYcMAUG3O4gyA')
+      options = {priority: 'high'}
+      options[:notification] = {}
+      options[:notification][:title] = 'proba'
+      options[:notification][:body] = 'funtzionatzen du'
+      # options = { data:{score: "123"}, collapse_key: "activate_GPS"}
       response = fcm.send(current_device.token, options)
+
 
       session[:GPS]= 'yes'
       render 'device/device'
@@ -42,11 +48,38 @@ class DeviceController < ApplicationController
 
   end
 
+
+
+  # def activate_GPS
+  #   if current_device
+  #
+  #     print 'kaixo'
+  #
+  #     base_uri = 'https://whereismyphone-2d1c3.firebaseio.com/'
+  #
+  #     firebase = Firebase::Client.new({url: base_uri})
+  #
+  #
+  #
+  #     response = firebase.push("todos", { :name => 'Pick the milk', :priority => 1 })
+  #     response.success? # => true
+  #     response.code # => 200
+  #     response.body # => { 'name' => "-INOQPH-aV_psbk3ZXEX" }
+  #     response.raw_body # => '{"name":"-INOQPH-aV_psbk3ZXEX"}'
+  #
+  #     session[:GPS]= 'yes'
+  #     render 'device/device'
+  #   else
+  #     redirect_to :root
+  #
+  #   end
+  # end
+
   def desactivate_GPS
     if current_device
       if session[:GPS].to_s=='yes'
 
-        fcm = FCM.new("AIzaSyAf8cH_rC19EbpWyiFRKezYcMAUG3O4gyA ")
+        fcm = FCM.new("AAAAUm2fpt4:APA91bHWeq-i1Dfc_ZgpQKqL5TIM3MBvXlMC4H8SVi8CDxvJKSEHHWgQvroxBO02XP0MGRZOZ6Y94Mz9auqfW_YzPzi4m0N7UUgvQDhsi0vLKsDKRWi5HpxuWS_nXxwiH0cNZs8fbhe9")
         options = { collapse_key: "desactivate_GPS"}
         response = fcm.send(current_device.token, options)
 
@@ -63,7 +96,7 @@ class DeviceController < ApplicationController
   def activate_alarm
     if current_device
 
-      fcm = FCM.new("AIzaSyAf8cH_rC19EbpWyiFRKezYcMAUG3O4gyA ")
+      fcm = FCM.new("AIzaSyAf8cH_rC19EbpWyiFRKezYcMAUG3O4gyA")
       options = { collapse_key: "activate_alarm"}
       response = fcm.send(current_device.token, options)
       session[:alarm]= 'yes'
@@ -78,7 +111,7 @@ class DeviceController < ApplicationController
     if current_device
       if session[:alarm].to_s=='yes'
 
-        fcm = FCM.new("AIzaSyAf8cH_rC19EbpWyiFRKezYcMAUG3O4gyA ")
+        fcm = FCM.new("AIzaSyAf8cH_rC19EbpWyiFRKezYcMAUG3O4gyA")
         options = { collapse_key: "desactivate_alarm"}
         response = fcm.send(current_device.token, options)
         session[:alarm]= 'no'
